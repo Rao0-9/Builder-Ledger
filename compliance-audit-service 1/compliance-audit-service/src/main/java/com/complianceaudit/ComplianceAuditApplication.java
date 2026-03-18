43b312f559a9c6be698eb3f608b6822576b19b0e
Rao <2479354@cognizant.com>
2026-03-18 22:09:34 +0530

    Add compliance audit service folder

---
+++ compliance-audit-service 1/compliance-audit-service/src/main/java/com/complianceaudit/ComplianceAuditApplication.java
@@ -0,0 +1,11 @@
+package com.complianceaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplianceAuditApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComplianceAuditApplication.class, args);
+    }
+}
